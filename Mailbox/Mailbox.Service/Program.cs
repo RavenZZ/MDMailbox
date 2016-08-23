89d82a36a039253ab86df517e4b8f7f53ea04715
Raven <ravenzyj@outlook.com>
2016-08-23 18:26:14 +0800

    add test push

--- Mailbox/Mailbox.Service/Program.cs
+++ Mailbox/Mailbox.Service/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Chroniton;
+using Chroniton.Jobs;
+using Chroniton.Schedules;
 using Mailbox.Business;
 using Mailbox.Dal;
 using Mailbox.Entity;
@@ -19,7 +23,7 @@ namespace Mailbox.Service
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             Console.WriteLine("Program Start");
-            
+
             var raven = new Entity.AccountBinding()
             {
                 AccountId = "f56d42b3-a75a-4eb0-885f-8b7a738581b9",
@@ -27,11 +31,37 @@ namespace Mailbox.Service
                 Mail = "zhuyingjunjun@foxmail.com",
                 Port = 993,
                 Server = "imap.qq.com",
-    
+                Token = "nnofhlbubmhbbcib",
                 Type = MailBindingType.Imap
             };
 
-            Imap imap = new Imap(raven);
+            var singularity = Singularity.Instance;
+
+            var job = new SimpleParameterizedJob<AccountBinding>((user, scheduledTime) =>
+            {
+                FetchUnseen(user);
+                Console.WriteLine($"scheduled: {scheduledTime.ToString()}");
+            });
+
+            var schedule = new EveryXTimeSchedule(TimeSpan.FromMinutes(1));
+
+            var scheduledJob = singularity.ScheduleParameterizedJob(schedule, job, raven, true);
+
+            singularity.Start();
+
+
+
+
+            Console.WriteLine("Press to Exit");
+            Console.ReadKey();
+        }
+
+
+
+
+        private static void FetchUnseen(AccountBinding binding)
+        {
+            Imap imap = new Imap(binding);
             Console.WriteLine("Imap Init Start");
             imap.Init();
             Console.WriteLine("Imap FetchNotSeen Start");
@@ -40,20 +70,21 @@ namespace Mailbox.Service
 
             foreach (KeyValuePair<UniqueId, string> pair in unseen)
             {
-                Console.WriteLine("Mail");
-                Console.WriteLine($"uid  {pair.Key}");
-                Console.WriteLine($"Subject {pair.Value}");
-
+              
                 var query =
                     from p in MailMongoContext.Instance.Notifications
-                    where p.UniqueId == pair.Key.ToString() && p.Email == raven.Mail
+                    where p.UniqueId == pair.Key.ToString() && p.Email == binding.Mail
                     select p;
-                if (!query.Any())
+               if (!query.Any())
                 {
-                    Console.WriteLine("Has Not Notice");
+                    Console.WriteLine("New Mail");
+                    Console.WriteLine("Mail");
+                    Console.WriteLine($"uid  {pair.Key}");
+                    Console.WriteLine($"Subject {pair.Value}");
+                    SendPush(string.Empty, binding.AccountId, $"新邮件:{pair.Value}");
                     var notification = new Notification()
                     {
-                        Email = raven.Mail,
+                        Email = binding.Mail,
                         Status = NotifyStatus.Success,
                         UniqueId = pair.Key.ToString()
                     };
@@ -62,16 +93,35 @@ namespace Mailbox.Service
                 }
                 else
                 {
-                    Console.WriteLine("Has Notice");
+                    //Console.WriteLine("Has Notice");
                 }
-
-        
             }
-            
 
-
-            Console.WriteLine("Press to Exit");
-            Console.ReadKey();
+            Console.WriteLine($"mail: {binding.Mail} fetching success");
         }
+
+
+
+        private static void SendPush(string token, string accountId, string message)
+        {
+            var json = "{\"access_token\":\"a8b86f3087dd4f52a8c60b191a4ac3d9\",\"account_id\":\""+ accountId + "\",\"message\":\""+ message + "\"}";
+
+            HttpContent content = new StringContent(json);
+            HttpClient client = new HttpClient();
+            var uri =
+                "https://api.mingdao.com/v1/message/Send_Inbox_System_Message?access_token=a8b86f3087dd4f52a8c60b191a4ac3d9&account_id=" +
+                accountId + "&message=" + message;
+            var task = client.PostAsync(uri, content);
+            var result = task.GetAwaiter().GetResult();
+
+        }
+
+
+
+
+
+
+
+
     }
 }
